33eb3566174fb2bf20bf02b1bd5498a0e72127c1
Ale Dominguez <aledomcru@hotmail.com>
2024-02-17 01:06:21 -0600

    practica3

---
+++ practica3.js
@@ -0,0 +1,49 @@
+// crea un objeto JSON que represente información sobre una persona (nombre, ciudad, edad)
+//y luego escribe una función que tome ese objeto como parámetro e imprima en la cosola
+//alguna de sus propiedades
+
+let persona1 = {
+   nombre:'Alex',
+   ciudad:'CDMX',
+   edad:'31',
+   cumpleaños: 12.14
+}
+const displayProperty = (obj, x) =>{
+    property= obj[x]
+    console.log(property)
+}
+
+displayProperty(persona1, ['nombre'])
+
+//crea un objeto JSON vacío y luego escribe una función que tome ese objeto como parametro
+//y agregue una nueva propiedad y su valor 
+
+let person2 ={
+}
+
+const addProperty = (obj, property, value) =>{
+    obj[property]='value'
+    return obj
+}
+
+addProperty(person2['nombre']= 'Alex')
+addProperty(person2['edad'] = '35')
+
+console.log(`La persona 2 se llama ${person2.nombre} y tiene ${person2.edad} años`)
+
+// //Define dos objetos JSON que regresen información sobre productos (nombre, precio, cantidad)
+// //Escribe una función que tome estos dos ojetos como parámetro y compare sus propiedades
+// //para determinar si son iguales o no. 
+
+// let producto1 = {
+//     nombre:'Mayonesa McCormick',
+//     precio:'$79',
+//     cantidad:'870g'
+// }
+
+// let producto2 = {
+//     nombre:'Mayonesa Hellmans',
+//     precio:'$92',
+//     cantidad:'800g',
+//     perdonoAPedrito:'sí'
+// }
